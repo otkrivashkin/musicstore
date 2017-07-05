6c8e960aff85e2172725f25dd7a3ece87edc6ec4
otkrivashkin <bondarenkoigor1985@gmail.com>
2017-07-05 10:43:17 +0300

    create Product model

---
+++ src/main/java/com/bin/otkrivashkin/model/Product.java
@@ -0,0 +1,13 @@
+package com.bin.otkrivashkin.model;
+
+public class Product {
+	
+	private String name;
+	private String category;
+	private String description;
+	private double price;
+	private String condition;
+	private String status;
+	private int unitInStock;
+	private String manufacturer;
+}
