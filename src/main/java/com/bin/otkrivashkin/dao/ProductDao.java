d6dce6ef6e8ce00c84b871e2107d3d83d821c102
otkrivashkin <bondarenkoigor1985@gmail.com>
2017-07-05 17:25:01 +0300

    implement products page

--- src/main/java/com/bin/otkrivashkin/dao/ProductDao.java
+++ src/main/java/com/bin/otkrivashkin/dao/ProductDao.java
@@ -11,19 +11,41 @@ public class ProductDao {
 
 	public List<Product> getProductList() {
 
-		Product prod1 = new Product();
-		prod1.setName("Guitart1");
-		prod1.setCategory("Instrument");
-		prod1.setDescription("This is a fender strat guitar!");
-		prod1.setPrice(1200);
-		prod1.setCondition("new");
-		prod1.setStatus("Active");
-		prod1.setUnitInStock(11);
-		prod1.setManufacturer("Fender");
+		Product product1 = new Product();
+		product1.setName("Guitart1");
+		product1.setCategory("Instrument");
+		product1.setDescription("This is a fender strat guitar!");
+		product1.setPrice(1200);
+		product1.setCondition("new");
+		product1.setStatus("Active");
+		product1.setUnitInStock(11);
+		product1.setManufacturer("Fender");
+		
+		Product product2 = new Product();
+		product2.setName("Guitart1");
+		product2.setCategory("Instrument");
+		product2.setDescription("This is a fender strat guitar!");
+		product2.setPrice(1200);
+		product2.setCondition("new");
+		product2.setStatus("Active");
+		product2.setUnitInStock(11);
+		product2.setManufacturer("Fender");
+		
+		Product product3 = new Product();
+		product3.setName("Guitart1");
+		product3.setCategory("Instrument");
+		product3.setDescription("This is a fender strat guitar!");
+		product3.setPrice(1200);
+		product3.setCondition("new");
+		product3.setStatus("Active");
+		product3.setUnitInStock(11);
+		product3.setManufacturer("Fender");
 
 		productList = new ArrayList<Product>();
 		
-		productList.add(prod1);
+		productList.add(product1);
+		productList.add(product2);
+		productList.add(product3);
 
 		return productList;
 	}
