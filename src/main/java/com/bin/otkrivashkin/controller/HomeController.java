2cbd84503225facda0445c103ac1334520547959
otkrivashkin <bondarenkoigor1985@gmail.com>
2017-07-05 22:20:13 +0300

    implement viewProduct.jsp with dummy data

--- src/main/java/com/bin/otkrivashkin/controller/HomeController.java
+++ src/main/java/com/bin/otkrivashkin/controller/HomeController.java
@@ -11,18 +11,24 @@ import com.bin.otkrivashkin.model.Product;
 
 @Controller
 public class HomeController {
-	
+
 	private ProductDao productDao = new ProductDao();
 
 	@RequestMapping("/")
 	public String home() {
 		return "home";
 	}
-	
+
 	@RequestMapping("/productList")
 	public String getProducts(Model model) {
-		List<Product> products = productDao.getProductList();	
+		List<Product> products = productDao.getProductList();
 		model.addAttribute("products", products);
 		return "productList";
+	}
+
+	@RequestMapping("/productList/viewProduct")
+	public String viewProduct() {
+
+		return "viewProduct";
 	}
 }
