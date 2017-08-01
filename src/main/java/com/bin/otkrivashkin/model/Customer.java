4c5943f88bb275118d85c7f71d4cc7be0f12b8f2
otkrivashkin <bondarenkoigor1985@gmail.com>
2017-08-01 12:00:33 +0300

    create Customer model

--- src/main/java/com/bin/otkrivashkin/model/Customer.java
+++ src/main/java/com/bin/otkrivashkin/model/Customer.java
@@ -1,7 +1,126 @@
 package com.bin.otkrivashkin.model;
 
-/**
- * Created by otkrivashkin on 01.08.2017.
- */
-public class Customer {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = -1381122192671672392L;
+
+    @Id
+    @GeneratedValue
+    private int customerId;
+
+    @NotEmpty(message = "The customer name must not be null.")
+    private String customerName;
+
+    @NotEmpty(message = "The customer email must not be null.")
+    private String customerEmail;
+
+    private String customerPhone;
+
+    @NotEmpty(message = "The customer user name must not be null.")
+    private String userName;
+
+    @NotEmpty(message = "The customer password must not be null.")
+    private String password;
+
+    private boolean enabled;
+
+    @OneToOne
+    @JoinColumn(name = "billingAddressId")
+    private BillingAddress billingAddres;
+
+    @OneToOne
+    @JoinColumn(name = "shippingAddressId")
+    private ShippingAddress shippingAddress;
+
+    @OneToOne
+    @JoinColumn(name = "cartId")
+    @JsonIgnore
+    private Cart cart;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public BillingAddress getBillingAddres() {
+        return billingAddres;
+    }
+
+    public void setBillingAddres(BillingAddress billingAddres) {
+        this.billingAddres = billingAddres;
+    }
+
+    public ShippingAddress getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(ShippingAddress shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
 }
