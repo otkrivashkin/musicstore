8adea7e6c44b2c1d23905750c839cadff4af0890
otkrivashkin <bondarenkoigor1985@gmail.com>
2017-08-02 10:58:05 +0300

    create AdminHome controller

---
+++ src/main/java/com/bin/otkrivashkin/controller/admin/AdminHome.java
@@ -0,0 +1,39 @@
+package com.bin.otkrivashkin.controller.admin;
+
+import com.bin.otkrivashkin.model.Product;
+import com.bin.otkrivashkin.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminHome {
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping
+    public String adminPage() {
+        return "admin";
+    }
+
+    @RequestMapping("/productInventory")
+    public String productInventory(Model model) {
+        List<Product> products = productService.getProductList();
+        model.addAttribute("products", products);
+        return "productInventory";
+    }
+
+    @RequestMapping("/customer")
+    public String customerManagement(Model model) {
+        // add customer service
+        return "customerManagement";
+    }
+
+
+
+}
