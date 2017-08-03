cc949d07eeee1be8d555bc77bc3ed73fb8757ea4
otkrivashkin <bondarenkoigor1985@gmail.com>
2017-08-03 10:14:32 +0300

    update controller.js

--- src/main/webapp/WEB-INF/resources/js/controller.js
+++ src/main/webapp/WEB-INF/resources/js/controller.js
@@ -4,14 +4,14 @@ var cartApp = angular.module("cartApp", []);
 
 cartApp.controller("cartCtrl", function ($scope, $http) {
 
-    $scope.refreshCart = function (cartId) {
+    $scope.refreshCart = function () {
         $http.get('/musicstore/rest/cart/' + $scope.cartId).success(function (data) {
             $scope.cart = data;
         });
     };
 
     $scope.clearCart = function () {
-      $http.delete('/musicstore/rest/cart/' + $scope.cartId).success($scope.refreshCart($scope.cartId));
+      $http.delete('/musicstore/rest/cart/' + $scope.cartId).success($scope.refreshCart());
     };
 
     $scope.initCartId = function (cartId) {
@@ -20,16 +20,25 @@ cartApp.controller("cartCtrl", function ($scope, $http) {
     };
 
     $scope.addToCart = function (productId) {
-      $http.put('/musicstore/rest/cart/add/' + productId).success(function (data) {
-          $scope.refreshCart($http.get('/musicstore/rest/cart/get/cartId'));
+      $http.put('/musicstore/rest/cart/add/' + productId).success(function () {
           alert("Product successfully added to the cart!");
       });
     };
 
     $scope.removeFromCart = function (productId) {
-        $http.put('/musicstore/rest/cart/remove/' + productId).success(function (data) {
-            $scope.refreshCart($http.get('/musicstore/rest/cart/cartId'));
+        $http.put('/musicstore/rest/cart/remove/' + productId).success(function () {
+            $scope.refreshCart();
         });
+    };
+
+    $scope.calGrandTotal = function () {
+        var grandTotal = 0;
+
+        for (var i = 0; i < $scope.cart.cartItems.length; i++) {
+            grandTotal += $scope.cart.cartItems[i].totalPrice;
+        }
+
+        return grandTotal;
     };
 
 });
